fd4cc6a07745c4c6cbcd97decc4524145e487e31
Anton Sineev <sineevanton@mail.ru>
2022-07-16 20:24:53 +0300

    Task 2 complete

---
+++ Task2/Task2.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+import random as rnd
+
+lenArr = int(input('Please enter the length of array: '))
+elementsProd = []
+arr = [rnd.randint(1, 10) for _ in range(lenArr)]
+loopRange = len(arr)//2 if len(arr) % 2 == 0 else len(arr)//2 + 1
+
+for i in range(0, loopRange):
+    elementsProd.append(arr[i] * arr[-(i+1)])
+print(arr)
+print(elementsProd)
